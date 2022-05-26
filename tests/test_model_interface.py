a8683f155edc06cf53e2feb9c0f126452f37be7d
angerhang <514140809@qq.com>
2022-05-26 01:02:47 +0100

    include unit tests

---
+++ tests/test_model_interface.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np
+
+
+def test_model_basic():
+    repo = 'OxWearables/ssl-wearables'
+    harnet10 = torch.hub.load(repo, 'harnet10', class_num=5, pretrained=True)
+    x = np.random.rand(1, 3, 300)
+    x = torch.FloatTensor(x)
+    y = harnet10(x)
+
+    assert y.shape[1] == 5
+
+
+def test_model_default():
+    repo = 'OxWearables/ssl-wearables'
+    harnet10 = torch.hub.load(repo, 'harnet10')
+    x = np.random.rand(1, 3, 300)
+    x = torch.FloatTensor(x)
+    y = harnet10(x)
+    assert y.shape[1] == 2
+
